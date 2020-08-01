1cb705815e33e1e2a42cdcf0aeddfe6e3b83ce76
Andres Rivera <soulkalir@gmail.com>
2020-08-01 00:19:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,27 +1,17 @@
-var playlist = new Object({
+var playlist = {
   
-  madonna:  'loca'
-  
-});
+  kirkorov : "edinstvennaya moya",
+  madonna: "frozen",
+};
 
 
 function updatePlaylist(playlist, artist, song){
-  
-  
-  playlist[artist] = song;
-  
-  
-  return playlist;
-  
+playlist[artist] = 'ladyGaga';
+return playlist;
 }
 
 
 function removeFromPlaylist(playlist, artist){
-  
-
-  delete playlist[artist];
-  
-  
+  delete playlist[artist]
   return playlist;
-  
 }
